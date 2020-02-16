7a3e359af2b81acda3d4a87fd03532058df13002
LiangDennis <18898513421@163.com>
2020-02-17 01:12:58 +0800

    elementui

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import './registerServiceWorker'
 import axios from 'axios'
 import '../mock/mock'
 
+import elementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(elementUI);
+
 Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false
